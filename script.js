7c94551f4f55a209caa99688cb32f1c367b4328a
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-09-29 13:04:31 -0600

    added comments

--- script.js
+++ script.js
@@ -27,9 +27,19 @@ generateBtn.addEventListener("click", writePassword);
 // 7. make sure each character type is present
 
 // TODO: define password length (n) and how each var works inside the password variable. 
+// var output = [upper + lower + number + special]
+// output is the entry value which includes the upper, lower, number, and special options set as true. 
 
 function generatePassword() {
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+  var numbers = `0, 1, 2, 3, 4, 5, 6, 7, 8, 9`
+  var specialCharacters = "!@#$%^&*().,<>?;:'`~"
   var entry = prompt("How many characters would you like your password to contain?", "Please choose from 8-128 characters");
+  // TODO: fix if someone selects 'cancel' from the prompt box   
+    // if (entry = false){
+    //   return;
+    // }
     if (entry < 8){
       alert("Must be more 7 characters");
       return;
@@ -39,21 +49,33 @@ function generatePassword() {
       return;
     }
     // TODO: fix this statement
-    // if (entry != Number){
+    // if (typeof entry != Number){
     //   alert("Must enter a number between 8-128");
     //   return;
     // }
     var upper = confirm("Click OKAY if you want UPPER-CASE letters. Click CANCEL if you do not.")
-    if (upper = false){
-    }
+      if (upper = true){
+       
+      }
 
     var lower = confirm("Click OKAY if you want lower-case letters. Click CANCEL if you do not.")
+      if (lower = true){
 
-    var numbers = confirm("Click OKAY if you want numbers. Click CANCEL if you do not.")
+      }
 
+    var number = confirm("Click OKAY if you want numbers. Click CANCEL if you do not.")
+      if (number = true){
+        // return Math.random(10)
+
+      }
     var special = confirm("Click OKAY if you want special characters. Click CANCEL if you do not.")
+      if (special = true){
 
+      }
 
+      // for (var i = 0; i < entry.length; i++) {
+      //      return array[upper + lower + number + special]   
+      // }
 
-
+    return [upper + lower + number + special]; passwordText
   }    
