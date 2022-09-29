6387044e63f3ee53a152cd40150cc0be2228242b
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-09-29 15:03:10 -0600

    added arrays

--- script.js
+++ script.js
@@ -31,10 +31,11 @@ generateBtn.addEventListener("click", writePassword);
 // output is the entry value which includes the upper, lower, number, and special options set as true. 
 
 function generatePassword() {
-  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
-  var numbers = `0, 1, 2, 3, 4, 5, 6, 7, 8, 9`
-  var specialCharacters = "!@#$%^&*().,<>?;:'`~"
+  var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '.', ',', '<', '>', '?', ';', ':', '~',]
+  var pattern = [upper, lower, number, special]
   var entry = prompt("How many characters would you like your password to contain?", "Please choose from 8-128 characters");
   // TODO: fix if someone selects 'cancel' from the prompt box   
     // if (entry = false){
@@ -48,34 +49,42 @@ function generatePassword() {
       alert("Must be less than 129 characters");
       return;
     }
+    console.log(entry)
     // TODO: fix this statement
     // if (typeof entry != Number){
     //   alert("Must enter a number between 8-128");
     //   return;
     // }
     var upper = confirm("Click OKAY if you want UPPER-CASE letters. Click CANCEL if you do not.")
+    console.log(upper)
       if (upper = true){
-       
+        return (Math.floor(Math.random() * upperCase.length));
       }
 
     var lower = confirm("Click OKAY if you want lower-case letters. Click CANCEL if you do not.")
+    console.log(lower)
+
       if (lower = true){
 
       }
 
     var number = confirm("Click OKAY if you want numbers. Click CANCEL if you do not.")
+    console.log(number)
+
       if (number = true){
         // return Math.random(10)
 
       }
     var special = confirm("Click OKAY if you want special characters. Click CANCEL if you do not.")
+    console.log(special)
+
       if (special = true){
 
       }
 
-      // for (var i = 0; i < entry.length; i++) {
-      //      return array[upper + lower + number + special]   
-      // }
-
-    return [upper + lower + number + special]; passwordText
+      for (var i = 0; i < entry; i++) {  
+        return (pattern); 
+      }
+// TODO: find out how to make the entry var be the parameters for length of the other variables. 
+    return;
   }    
